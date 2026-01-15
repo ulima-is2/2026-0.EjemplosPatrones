823d6437ca9aa0cb09151383d27fe2b7f49b6def
Hernan <hernan@jsatch.com>
2026-01-14 20:39:53 -0500

    Comenzando la implementacion del patron adapter

---
+++ patrones/adapter/LibreriaAudio.java
@@ -0,0 +1,15 @@
+package patrones.adapter;
+
+public class LibreriaAudio {
+    public void start() {
+        System.out.println("Se inicia la reproduccion con LA1");
+    }
+
+    public void pause() {
+        System.out.println("Se pausea la reproduccion con LA1");
+    }
+
+    public void stop() {
+        System.out.println("Se detiene la reproduccion con LA1");
+    }
+}
